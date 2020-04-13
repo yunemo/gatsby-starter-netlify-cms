b34063fa17c4563adeaf3c9afece2ba4ed153932
yunemo <eatordesign@gmail.com>
2020-04-13 15:51:10 +0900

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -23,7 +23,7 @@ export const BlogPostTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
+              　てすと　{title}　
             </h1>
             <p>{description} てすと</p>
             <PostContent content={content} />
