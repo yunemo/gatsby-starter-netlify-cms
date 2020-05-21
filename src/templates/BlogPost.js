d8518c18acd36c91e4fb327bd5dcb05062d4d7f8
nemo <>
2020-05-21 23:23:11 +0900

    no message

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -11,6 +11,59 @@ import facebook from '../img/icon-fb.svg'
 import twitter from '../img/icon-tw.svg'
 import CategoryColors from '../data/CategoryColors'
 
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  category,
+  title,
+  featuredimage,
+  figcaption,
+  date
+}) => {
+  const PostContent = contentComponent || content
+  return (
+    <section className="post">
+      <div className="post_header">
+        <div className="post_subline">
+          <div className="post_date">{date} </div>
+          <div className="post_category" style={
+            CategoryColors[snakeCase(category)] ?
+              { color: CategoryColors[snakeCase(category)] }
+              : null
+          }>
+            {category}     
+          </div>
+        </div>
+
+        <div className="post_title">{title}</div>
+        <div className="post_social">
+          <a className="post_social_link" href="https://www.facebook.com/ZeBrand.official/" target="_blank" alt="facebook">
+            <img src={facebook} />
+          </a>
+          <a className="post_social_link" href="https://twitter.com/ZeBrandOfficial/" target="_blank" alt="twitter">
+            <img src={twitter} />
+          </a>
+        </div>
+        {
+          featuredimage ? figcaption ? (
+            <div className="post_kv">
+              <Img fluid={featuredimage} alt={title} />
+              <div className="post_caption">{figcaption}</div>
+            </div>
+          ) : (
+              <div className="post_kv">
+                <Img fluid={featuredimage} alt={title} />
+              </div>
+            ) : null
+        }
+      </div>
+      <PostContent content={content} className="post_content"/>
+      <RelatedPost category={category} title={title}/>
+    
+    </section>
+  )
+}
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
@@ -106,63 +159,3 @@ export const pageQuery = graphql`
   }
 `
 export default BlogPost
-
-export const BlogPostTemplate = ({
-  content,
-  contentComponent,
-  description,
-  category,
-  title,
-  featuredimage,
-  figcaption,
-  date
-}) => {
-  const PostContent = contentComponent || content
-
-  return (
-    <section className="post">
-      <div className="post_header">
-        <div className="post_subline">
-          <div className="post_date">{date} </div>
-          <div className="post_category" style={
-            CategoryColors[snakeCase(category)] ?
-              { color: CategoryColors[snakeCase(category)] }
-              : null
-          }>
-            {category}     
-          </div>
-        </div>
-
-        <div className="post_title">{title}</div>
-
-        <div className="post_social">
-          <a className="post_social_link" href="https://www.facebook.com/ZeBrand.official/" target="_blank" alt="facebook">
-            <img src={facebook} />
-          </a>
-          <a className="post_social_link" href="https://twitter.com/ZeBrandOfficial/" target="_blank" alt="twitter">
-            <img src={twitter} />
-          </a>
-        </div>
-
-        {
-          featuredimage ? figcaption ? (
-            <div className="post_kv">
-              <Img fluid={featuredimage} alt={title} />
-              <div className="post_caption">{figcaption}</div>
-            </div>
-          ) : (
-              <div className="post_kv">
-                <Img fluid={featuredimage} alt={title} />
-              </div>
-            ) : null
-        }
-
-      </div>
-
-      <PostContent content={content} className="post_content"/>
-
-      <RelatedPost category={category} title={title}/>
-    
-    </section>
-  )
-}
