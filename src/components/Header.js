520677abf4c6ade7e91fdf4d86ac7132719ee431
nemo <>
2020-05-22 08:12:58 +0900

    no message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { useMediaQuery } from "react-responsive";
 import DesktopHeader from './DesktopHeader'
 import MobileHeader from './MobileHeader'
 
 const Header = () => {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 1030px)'
-  })
   return (
     <div>  
-      { isDesktop ? <DesktopHeader /> : <MobileHeader /> }
+      <DesktopHeader />
+      <MobileHeader />
     </div>
   )
 }
