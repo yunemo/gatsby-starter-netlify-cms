e151cc29742e7f397b5f4ea99109f5759ef4c170
nemo <>
2020-05-22 11:19:34 +0900

    no message

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -28,7 +28,7 @@ export const BlogPostTemplate = ({
   const dateList = date.toUTCString().split(" ")
   const dateStr = `${dateList[2]} ${dateList[1]},${dateList[3]}`;
 
-  const imageStr = featuredimage.toString()
+  const imageStr = JSON.stringify(featuredimage)
   return (
     <section className="post">
       <div className="post_header">
